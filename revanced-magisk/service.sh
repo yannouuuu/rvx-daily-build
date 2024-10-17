646751a9274f698bccb7956bc257fc3e0dfda548
Yann Renard <76118368+yannouuuu@users.noreply.github.com>
2024-10-17 23:47:58 +0200

    rebase: latest source

--- revanced-magisk/service.sh
+++ revanced-magisk/service.sh
@@ -6,7 +6,7 @@ RVPATH=/data/adb/rvhc/${MODDIR##*/}.apk
 until [ "$(getprop sys.boot_completed)" = 1 ]; do sleep 1; done
 until [ -d "/sdcard/Android" ]; do sleep 1; done
 while
-	BASEPATH=$(pm path "$PKG_NAME")
+	BASEPATH=$(pm path "$PKG_NAME" 2>&1 </dev/null)
 	svcl=$?
 	[ $svcl = 20 ]
 do sleep 2; done
@@ -23,7 +23,7 @@ if [ $svcl != 0 ]; then
 fi
 BASEPATH=${BASEPATH##*:} BASEPATH=${BASEPATH%/*}
 if [ ! -d "$BASEPATH/lib" ]; then
-	err "zygote crashed (fix your ROM)"
+	err "system force-rebooted (fix your ROM)"
 	exit
 fi
 VERSION=$(dumpsys package "$PKG_NAME" | grep -m1 versionName) VERSION="${VERSION#*=}"
